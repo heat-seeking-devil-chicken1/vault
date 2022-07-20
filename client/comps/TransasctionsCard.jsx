4541ad5cef8d63d044d466fa2fe234c1bf97c03a
swittuthcoding <swittuthcoding@gmail.com>
2022-07-20 16:58:32 -0400

    show transactions when user logs in

--- client/comps/TransasctionsCard.jsx
+++ client/comps/TransasctionsCard.jsx
@@ -1,8 +1,72 @@
-import React from "react";
+import React, { useContext } from "react";
+import { InfoContext } from "../containers/MainContainer.jsx";
 import { Paper, Typogrpahy, Avatar, Box, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 
 export function TransactionsCard() {
-  
+  const [userInfo, setUserInfo] = useContext(InfoContext);
+  const transactionArray = [];
+  const transactions = userInfo.transactions;
+
+  if (transactions.length > 0) {
+    for (let trans of transactions) {
+      transactionArray.push(
+        <motion.div
+          animate={{
+            opacity: 1,
+            width: "100%",
+            overflowY: "auto",
+          }}
+          initial={{
+            opacity: 0,
+            width: "100%",
+            overflowY: "auto",
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              height: "max-content",
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              overflowY: "auto",
+            }}
+          >
+            <Box
+              sx={{
+                width: "50%",
+                height: "max-content",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "flex-start",
+                width: "100%",
+                overflowY: "auto",
+              }}
+            >
+              <Typography>{trans.amount}</Typography>
+            </Box>
+            <Box
+              sx={{
+                width: "50%",
+                height: "max-content",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "flex-end",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Typography>{trans.dates.split("T")[0]}</Typography>
+            </Box>
+          </Box>
+        </motion.div>
+      );
+    }
+  }
+
   return (
     <Paper
       elevation={12}
@@ -10,10 +74,10 @@ export function TransactionsCard() {
         width: "100%",
         height: "100%",
         display: "flex",
-        flexDirection: "row",
+        flexDirection: "column",
         padding: "10px",
-        alignItems: "flex-start",
-        justifyContent: "center",
+        alignItems: "center",
+        justifyContent: "flex-start",
         padding: "10px",
         borderRadius: "20px",
       }}
@@ -25,6 +89,21 @@ export function TransactionsCard() {
       >
         TRANSACTIONS
       </Typography>
+      {userInfo.loggedIn && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "100%",
+            overflowY: "auto",
+          }}
+        >
+          {transactionArray}
+        </Box>
+      )}
     </Paper>
   );
 }
