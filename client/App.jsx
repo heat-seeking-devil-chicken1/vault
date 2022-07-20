b2f251f86fe26ceeeb88b101457c484aa1c56e0c
swittuthcoding <swittuthcoding@gmail.com>
2022-07-19 20:09:24 -0400

    made changes

--- client/App.jsx
+++ client/App.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MainContainer from '../client/containers/MainContainer.jsx';
 //import styles from './styles.scss';
-
+// import router
 const App = () => {
   return (
+    // wrap around browser
     <div className='app'>
       <MainContainer />
     </div>
