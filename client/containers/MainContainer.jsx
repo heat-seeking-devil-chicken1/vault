8096a6cea395ee58ccd4c26381d32d8ca9263e87
swittuthcoding <swittuthcoding@gmail.com>
2022-07-20 10:30:43 -0400

    udpate functional component

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect, useContext, createContext } from "react";
 import "../stylesheets/styles.scss";
 import Header from "../comps/Header.jsx";
 import Footer from "../components/Footer.jsx";
@@ -8,67 +8,67 @@ import { WelcomeUser } from "../comps/WelcomeUser.jsx";
 import { TransactionsCard } from "../comps/TransasctionsCard.jsx";
 import { CashflowCard } from "../comps/CashflowCard.jsx";
 import { AnnualForecastCard } from "../comps/AnnualForecastCard.jsx";
-class MainContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedInUser: 1,
-      transactions: [],
-      categories: [],
-      balance: [],
-      sumArray: [],
-      monthlyIncome: 5000,
-      // synced: false,
-    };
-    // this.updateSynced = this.updateSynced.bind(this);
-  }
+export function MainContainer() {
+  const InfoContext = createContext();
 
-  componentDidMount() {
-    // make call to our endpoint and populate
-    fetch("/transactions/all/" + this.state.loggedInUser)
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          ...this.state,
-          categories: data.categories,
-          sum: data.sum,
-        });
-      });
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     loggedInUser: 1,
+  //     transactions: [],
+  //     categories: [],
+  //     balance: [],
+  //     sumArray: [],
+  //     monthlyIncome: 5000,
+  //     // synced: false,
+  //   };
+  //   // this.updateSynced = this.updateSynced.bind(this);
+  // }
 
-  render() {
-    return (
-      <>
-        <div className="grid-container">
-          <Header />
-          <div id="container">
-            <div class="Cash-Flow">
-              <CashflowCard></CashflowCard>
-            </div>
-            <div class="Annual-Forecast">
-              <AnnualForecastCard></AnnualForecastCard>
-            </div>
-            <div class="Monthly-Spendings">
-              <MonthlySpendingCard></MonthlySpendingCard>
-            </div>
-            <div class="Category-Spendings">
-              <CategorySpendingCard></CategorySpendingCard>
-            </div>
-            <div class="Transactions">
-              <TransactionsCard></TransactionsCard>
-            </div>
-            <div class="User-Area">
-              <WelcomeUser></WelcomeUser>
-            </div>
+  // componentDidMount() {
+  //   // make call to our endpoint and populate
+  //   fetch("/transactions/all/" + this.state.loggedInUser)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       this.setState({
+  //         ...this.state,
+  //         categories: data.categories,
+  //         sum: data.sum,
+  //       });
+  //     });
+  // }
+
+  return (
+    <>
+      <div className="grid-container">
+        <Header />
+        <div id="container">
+          <div class="Cash-Flow">
+            <CashflowCard></CashflowCard>
           </div>
-
-          <div className="footer">
-            <Footer />
+          <div class="Annual-Forecast">
+            <AnnualForecastCard></AnnualForecastCard>
+          </div>
+          <div class="Monthly-Spendings">
+            <MonthlySpendingCard></MonthlySpendingCard>
+          </div>
+          <div class="Category-Spendings">
+            <CategorySpendingCard></CategorySpendingCard>
+          </div>
+          <div class="Transactions">
+            <TransactionsCard></TransactionsCard>
+          </div>
+          <div class="User-Area">
+            <WelcomeUser></WelcomeUser>
           </div>
         </div>
-      </>
-    );
-  }
+
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MainContainer;
